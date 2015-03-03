c2885bb52e4fcb8866c015d25cd36b9219067269
Elie Prudhomme <eprudhomme@v3d.fr>
2015-03-03 10:36:42 +0100

    Refactor generate.php for better shipping with composer

--- bin/generate.php
+++ bin/generate.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__."/../vendor/autoload.php";
+(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
 use Avro\Generator\ProtocolGenerator;
 
